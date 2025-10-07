1ae684dd5d65e502799e89600a76152bfdade0bc
413X-A <wollekalexander@gmail.com>
2025-10-08 00:48:54 +0200

    Update script_gebaeude.js

--- script_gebaeude.js
+++ script_gebaeude.js
@@ -760,8 +760,8 @@ function betriebsmittelAnzeigen() {
 
                 // 📝 Anzeige aktualisieren
                 if (info.wasser > 0) lagerText.textContent = `Im Lager: ${Math.floor(info.lager.wasser)} Wasser / ${daten.betriebsmittel_lager["wasser"]}`;
-                else if (info.energie > 0) lagerText.textContent = `Im Lager: ${Math.floor(info.lager.energie)} Energie / ${(daten.betriebsmittel_lager["energie"]}`;
-                else if (info.gold > 0) lagerText.textContent = `Im Lager: ${Math.floor(info.lager.gold)} Gold / ${(daten.betriebsmittel_lager["gold"]}`;
+                else if (info.energie > 0) lagerText.textContent = `Im Lager: ${Math.floor(info.lager.energie)} Energie / ${daten.betriebsmittel_lager["energie"]}`;
+                else if (info.gold > 0) lagerText.textContent = `Im Lager: ${Math.floor(info.lager.gold)} Gold / ${daten.betriebsmittel_lager["gold"]}`;
 
                 // Countdown anzeigen
                 const rest = Math.max(0, info.fertiggewachsen - jetzt);
@@ -881,6 +881,7 @@ function betriebsmittelAnzeigen() {
         document.body.appendChild(overlay);
     }
 }
+
 
 
 
